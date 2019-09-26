b8da084d1caebc6362c767291d4f7d4114433c14
bram <bram.sierag@isatis-health.nl>
2019-09-26 20:32:21 +0200

    fix for docker chrome

---
+++ scripts/install-chrome.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -xe
+
+# Install essential tools
+apt-get update -q
+apt-get upgrade -y
+apt-get install -y \
+    libappindicator1 \
+    fonts-liberation \
+    libasound2 \
+    libgconf-2-4 \
+    libnspr4 \
+    libxss1 \
+    libnss3 \
+    xdg-utils
+
+export CHROME_BIN=/usr/bin/google-chrome
+export DISPLAY=:99.0
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome*.deb
+
+sh /scripts/cleanup.sh
